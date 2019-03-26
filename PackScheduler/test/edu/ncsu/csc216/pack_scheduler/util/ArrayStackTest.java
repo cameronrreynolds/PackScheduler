5bc7fd25cff5f0103534e68fda6b64f09ca56025
crreynol <crreynol@ncsu.edu>
2019-03-25 20:33:45 -0400

    Testing ArrayStack and LinkedStack

---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/ArrayStackTest.java
@@ -0,0 +1,89 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.pack_scheduler.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.EmptyStackException;
+
+import org.junit.*;
+
+
+/**
+ * @author Cameron, Michael, Matthew
+ * @param <E>
+ *
+ */
+public class ArrayStackTest {
+
+	@Test
+	public void testPush() {
+		LinkedStack<String> stack = new LinkedStack<String>(2);
+		try {
+			stack.push("First");
+			assertEquals("First", stack.pop());
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		try {
+			stack.push("First");
+			stack.push("Top");
+			assertEquals("Top", stack.pop());
+			assertEquals("First", stack.pop());
+			assertTrue(stack.isEmpty());
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		try {
+			stack.push("First");
+			stack.push("Second");
+			stack.push("Third");
+			fail();
+		} catch (IllegalArgumentException e) {
+
+		}
+	}
+	
+	@Test
+	public void testPop() {
+		LinkedStack<String> stack = new LinkedStack<String>(10);
+		try {
+			stack.push("First");
+			stack.push("Second");
+			assertEquals("Second", stack.pop());
+			assertEquals("First", stack.pop());
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		try {
+			stack.push("First");
+			stack.push("Second");
+			assertEquals("Second", stack.pop());
+			assertEquals("First", stack.pop());
+			stack.pop();
+			fail();
+		} catch (EmptyStackException e) {
+		
+		}
+	}
+	
+	@Test
+	public void testIsEmpty() {
+		LinkedStack<String> stack = new LinkedStack<String>(10);
+		assertEquals(0, stack.size());
+	}
+	
+	@Test
+	public void testSetCapacity() {
+		LinkedStack<String> stack = new LinkedStack<String>();
+		try {
+			stack.setCapacity(-1);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertEquals(0, stack.size());
+		}
+	}
+}
