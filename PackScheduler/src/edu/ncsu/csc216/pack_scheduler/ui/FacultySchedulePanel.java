2f57f1af3fa47ddffbe72b42119777cf0397ded3
mcstric4 <mcstric4@ncsu.edu>
2019-04-15 17:29:44 -0400

    Created FacultySchedulePanel

---
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/ui/FacultySchedulePanel.java
@@ -0,0 +1,11 @@
+package edu.ncsu.csc216.pack_scheduler.ui;
+
+public class FacultySchedulePanel {
+
+	/** ID number used for object serialization. */
+	private static final long serialVersionUID = 1L;
+
+	
+	
+	
+}
